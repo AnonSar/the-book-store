b6d904a8cbc548b1d70aa30dd0744f60e43c27a3
ANMOL SARRAF <asar0011@student.monash.edu>
2020-07-30 13:27:58 +1000

    Updates

---
+++ precache-manifest.022db56557860ece067352e1fcdd8900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0d79a99dc1d40d91d54b2f26bc9923",
+    "url": "/the-book-store/index.html"
+  },
+  {
+    "revision": "c25aa30e7eda2be42f30",
+    "url": "/the-book-store/static/css/main.385e767e.chunk.css"
+  },
+  {
+    "revision": "9582aec4f77f38c243f5",
+    "url": "/the-book-store/static/js/2.80c09d07.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/the-book-store/static/js/2.80c09d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25aa30e7eda2be42f30",
+    "url": "/the-book-store/static/js/main.5271745e.chunk.js"
+  },
+  {
+    "revision": "c1efa06760ac6a170e36",
+    "url": "/the-book-store/static/js/runtime-main.04343507.js"
+  }
+]);
